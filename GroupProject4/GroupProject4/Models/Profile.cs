0d0b43a5f7beb0f020f8960e1088afd7c387fd2e
Tony Baker <tonybaker23@ou.edu>
2019-04-22 11:22:47 -0500

    commit 1

--- GroupProject4/GroupProject4/Models/Profile.cs
+++ GroupProject4/GroupProject4/Models/Profile.cs
@@ -7,6 +7,16 @@ namespace GroupProject4.Models
 {
     public class Profile
     {
-
+        public string ProfilePicture { get; set; }
+        public string HomeTown { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Education { get; set; }
+        public string Job { get; set; }
+        public string Pets { get; set; }
+        public string LookingFor { get; set; }
+        public string Excersise { get; set; }
+        
     }
 }
